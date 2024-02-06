e93665b4a15ee1874e49db11645cf3d8341715bf
Nikolaykrasnov <117943052+Nikolaykrasnov@users.noreply.github.com>
2024-02-06 18:36:15 +0500

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,5 +1,7 @@
-a=set(input().split())
-
-b=set(input().split())
-
-print (len(a.intersection(b)))
+n = int(input())
+a = []
+for i in range(n):
+    a.append(int(input()))
+a.reverse()
+for x in a:
+    print(x)
