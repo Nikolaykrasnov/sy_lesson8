7817d7a2eeb875b66d445643bab90de665cc3bfa
Nikolaykrasnov <117943052+Nikolaykrasnov@users.noreply.github.com>
2024-02-06 18:37:38 +0500

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,13 +1,19 @@
-a=set()
+m = int(input())
+n = int(input())
+weights = []
+for i in range(n):
+    weights.append(int(input()))
+weights.sort()
 
-for i in input().split():
+boats = 0
+i = 0
+j = n - 1
+while i <= j:
+    if weights[i] + weights[j] <= m:
+        i += 1
+        j -= 1
+    else:
+        j -= 1
+    boats += 1
 
-    if i not in a:
-
-        a.add(i)
-
-    print('NO')
-
-else:
-
-    print('YES')
+print(boats)
